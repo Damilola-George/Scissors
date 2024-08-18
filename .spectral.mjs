0ea2a453ff755fd32b6e034907138799c6f091e6
Damilola George <tammygeorge97@gmail.com>
2024-08-18 23:56:25 +0100

    Added 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI1NTk3OQ/spectral.js?branch_slug=main&token=232bf5e1-f0e3-45bf-86c3-e9407d5a8f8c";
+export default { extends: ruleset };
